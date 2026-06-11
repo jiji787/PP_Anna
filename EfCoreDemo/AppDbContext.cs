6ba89bfcb526630c5dbfdbf5a36f8464241dda6b
Anna Valeryevna <ji.ji0001q@gmail.com>
2026-06-11 10:24:02 +0300

    Исправлен конфликт миграций EF Core

--- EfCoreDemo/AppDbContext.cs
+++ EfCoreDemo/AppDbContext.cs
@@ -8,19 +8,25 @@ public class AppDbContext : DbContext
     public DbSet<Product> Products { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<OrderStatus> OrderStatuses { get; set; }   // <-- ЭТУ СТРОКУ ДОБАВЬТЕ
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        // Используй ТУ ЖЕ строку подключения, что и в основном приложении
         optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Золотце;Username=postgres;Password=postgres890");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // Дополнительные настройки, если нужно
         modelBuilder.Entity<Order>()
             .HasOne(o => o.User)
             .WithMany()
             .HasForeignKey(o => o.UserId);
+
+        modelBuilder.Entity<OrderStatus>().HasData(
+            new OrderStatus { Id = 1, Name = "новый" },
+            new OrderStatus { Id = 2, Name = "оплачен" },
+            new OrderStatus { Id = 3, Name = "отправлен" },
+            new OrderStatus { Id = 4, Name = "завершён" }
+        );
     }
 }
--- EfCoreDemo/obj/Debug/net10.0/EfCoreDemo.AssemblyInfo.cs
+++ EfCoreDemo/obj/Debug/net10.0/EfCoreDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EfCoreDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee5ccce9e096a7f2ce09a1ce1360f0d5e010f8ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+073016a3c611e6521b4ae16a2ecaeaad1507821c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EfCoreDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EfCoreDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
